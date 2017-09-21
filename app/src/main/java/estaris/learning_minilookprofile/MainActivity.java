8b3b8dcc944cef75d2f890dc4875807bb611e6b0
Jepoy <jeffestaris@gmail.com>
2017-09-21 19:12:08 +0800

    Initial commit

---
+++ app/src/main/java/estaris/learning_minilookprofile/MainActivity.java
@@ -0,0 +1,18 @@
+/*
+*   This is a practice code whereas a user can click on a picture, then shows its profile information like social
+*   media applications will do. This is created by me, the one and only Jep Estaris.
+*   THIS IS INTENDED FOR STUDY PURPOSES ONLY. RE-USE IT AT YOUR OWN RISK.
+*   */
+package estaris.learning_minilookprofile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
